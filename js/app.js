2743f075b38d8c5b55458cad4f4468b4426201a2
Vladimir Gasic <vgashic@gmail.com>
2016-07-12 01:33:11 +0200

    some javascrit

--- js/app.js
+++ js/app.js
@@ -4,16 +4,37 @@ $("#results-header").hide();
 
 $("#small-random-button").toggleClass("invisible");
 
+function showResults(item, index) {
+	$("#search-results").append(item);
+}
+
+function getResults(textToSearch) {
+	var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + textToSearch;
+
+
+	$http.jsonp(url)
+		.success(function (data) {
+			var results = data.query.pages;
+
+			results.forEach(showResults);
+		});
+	return false;
+
+}
+
 
 
 $("#search-form").submit(function () {
-    $("#main-screen").hide();
+	$("#main-screen").hide();
 
-    $("#results-header").show();
-    $("#search-results").show();
+	$("#results-header").show();
+	$("#search-results").show();
 
-    $("#search-form-wrapper").appendTo("#results-header");
-    $(".search-buttons").remove();
-    $("#small-random-button").toggleClass("invisible");
-    return false;
+	$("#search-form-wrapper").appendTo("#results-header");
+	$(".search-buttons").remove();
+	$("#small-random-button").toggleClass("invisible");
+
+	getResults();
+
+	return false;
 });
